8eb96b55007a04727e6306851ae1bb3949f93d1d
Daniel Taub <dmtaub@gmail.com>
2025-03-29 14:15:31 -0400

    remove brunch coffee

--- app/initialize.js
+++
@@ -1,7 +0,0 @@
-App = require('app');
-
-document.addEventListener('DOMContentLoaded', function() {
-  // do your setup here
-  console.log('Initialized brunch app');
-  window.game = new App();
-});
