6b2ee7495e8618ac9ff332a2a181a0d76a068c36
Daniel Taub <dmtaub@gmail.com>
2025-03-29 18:38:43 -0400

    restore key handlers

--- src/controls.js
+++ src/controls.js
@@ -9,78 +9,153 @@ export class Controls {
     this.linearFactor = 0.3 * State.slow_factor;
     this.rotationalFactor = 10 * State.slow_factor;
 
-    // Set up keyboard controls
+    // Set up keyboard controls with repeat
     this.keys = {};
-    window.addEventListener('keydown', (e) => this.keys[e.key] = true);
-    window.addEventListener('keyup', (e) => this.keys[e.key] = false);
+    this.keyIntervals = {};
+
+    window.addEventListener('keydown', (e) => {
+      if (!this.keys[e.key]) {
+        this.keys[e.key] = true;
+        this.handleKeyPress(e.key);
+        // Start interval for held keys
+        this.keyIntervals[e.key] = setInterval(() => {
+          this.handleKeyPress(e.key);
+        }, 16); // ~60fps - TODO: confirm, make dynamic
+      }
+    });
+
+    window.addEventListener('keyup', (e) => {
+      this.keys[e.key] = false;
+      if (this.keyIntervals[e.key]) {
+        clearInterval(this.keyIntervals[e.key]);
+        delete this.keyIntervals[e.key];
+      }
+    });
   }
 
   jump(scale = 2) {
-    // Note: We'll need to implement physics-based velocity
-    // For now, we'll just move the ball up
-    this.player.shape.position.y += this.jumpVelocity * scale;
+    if (!this.player.rigidBody) return;
+    const velocity = this.player.rigidBody.linvel();
+    const newVel = {
+      x: velocity.x,
+      y: this.jumpVelocity * scale,
+      z: velocity.z
+    };
+    this.player.rigidBody.setLinvel(newVel, true);
   }
 
   setupActions(terrain) {
+    this.terrain = terrain;
     const doAfterDelay = () => {
       this.jump();
     };
     const delay = State.transition_time / 2;
 
-    // Set up key handlers
     window.addEventListener('keyup', (e) => {
       switch(e.key.toLowerCase()) {
-        case 'w':
-          terrain.setTarget(2);
-          if (terrain.lastScale < 1) {
-            this.jump();
+        case '=':
+          this.terrain.setTarget(2); // "forward"
+          if (this.terrain.lastScale < 1) {
+            // this.jump(); - was added to help a bug in the past
             setTimeout(doAfterDelay, delay);
           }
           break;
-        case 'a':
-          terrain.setTarget(terrain.lastScale);
-          if (terrain.lastScale > 1) {
-            this.jump();
+        case '-':
+          this.terrain.setTarget(this.terrain.lastScale);
+          if (this.terrain.lastScale > 1) {
+            // this.jump(); - was added to help a bug in the past
             setTimeout(doAfterDelay, delay);
           }
           break;
-        case 's':
-          terrain.setTarget(0);
+        case '0':
+          this.terrain.setTarget(0);
           break;
-        case 'd':
-          terrain.setTarget(1);
+        case '1':
+          this.terrain.setTarget(1);
           break;
         case 'e':
-          terrain.setTarget(Math.random());
+          // legacy for Onyi and Steven
+          this.terrain.setTarget(Math.random());
           break;
       }
     });
   }
 
-  moveWithKeys() {
-    if (State.disable_arrows || !this.player.rigidBody) return;
+  isPlayerGrounded() {
+    // For Rapier, we'll need to check collision events
+    // This is a placeholder - you'll need to implement proper ground detection
+    // return this.player.rigidBody.collider(0).numContacts() > 0;
+    return true;
+  }
 
-    const force = this.linearFactor;
-    const torque = this.rotationalFactor;
+  handleKeyPress(key) {
+    if (!this.player.rigidBody || State.disable_arrows) return;
 
-    if (this.keys['ArrowRight']) {
-      this.player.rigidBody.applyImpulse({ x: force, y: 0, z: 0 }, true);
-      this.player.rigidBody.applyTorqueImpulse({ x: -torque, y: 0, z: -torque }, true);
+    const velocity = this.player.rigidBody.linvel();
+    const angVelocity = this.player.rigidBody.angvel();
+    let newVel = { x: velocity.x, y: velocity.y, z: velocity.z };
+    let newAngVel = { x: angVelocity.x, y: angVelocity.y, z: angVelocity.z };
+
+    const contactGround = this.isPlayerGrounded();
+
+    switch (key) {
+      case 'ArrowRight':
+      case 'd':
+        newVel.x += this.linearFactor;
+        newVel.z -= this.linearFactor;
+        if (contactGround) {
+          newAngVel.x -= this.rotationalFactor;
+          newAngVel.z -= this.rotationalFactor;
+        }
+        break;
+      case 'ArrowLeft':
+      case 'a':
+        newVel.x -= this.linearFactor;
+        newVel.z += this.linearFactor;
+        if (contactGround) {
+          newAngVel.x += this.rotationalFactor;
+          newAngVel.z += this.rotationalFactor;
+        }
+        break;
+      case 'ArrowUp':
+      case 'w':
+        newVel.x -= this.linearFactor;
+        newVel.z -= this.linearFactor;
+        if (contactGround) {
+          newAngVel.x -= this.rotationalFactor;
+          newAngVel.z += this.rotationalFactor;
+        }
+        break;
+      case 'ArrowDown':
+      case 's':
+        newVel.x += this.linearFactor;
+        newVel.z += this.linearFactor;
+        if (contactGround) {
+          newAngVel.x += this.rotationalFactor;
+          newAngVel.z -= this.rotationalFactor;
+        }
+        break;
+      case ' ':
+        if (contactGround) {
+          newVel.y = this.jumpVelocity;
+        }
+        break;
     }
-    if (this.keys['ArrowLeft']) {
-      this.player.rigidBody.applyImpulse({ x: -force, y: 0, z: 0 }, true);
-      this.player.rigidBody.applyTorqueImpulse({ x: torque, y: 0, z: torque }, true);
-    }
-    if (this.keys['ArrowUp']) {
-      this.player.rigidBody.applyImpulse({ x: 0, y: 0, z: -force }, true);
-      this.player.rigidBody.applyTorqueImpulse({ x: -torque, y: 0, z: -torque }, true);
-    }
-    if (this.keys['ArrowDown']) {
-      this.player.rigidBody.applyImpulse({ x: 0, y: 0, z: force }, true);
-      this.player.rigidBody.applyTorqueImpulse({ x: torque, y: 0, z: torque }, true);
-    }
-    if (this.keys[' ']) {
-      this.player.rigidBody.applyImpulse({ x: 0, y: this.jumpVelocity, z: 0 }, true);
-    }
+
+    // Clamp velocities
+    newVel = this.clampVector(newVel);
+    newAngVel = this.clampVector(newAngVel);
+
+    // Apply velocities
+    this.player.rigidBody.setLinvel(newVel, true);
+    this.player.rigidBody.setAngvel(newAngVel, true);
+  }
+
+  clampVector(vec) {
+    return {
+      x: Math.max(State.min_vector.x, Math.min(State.max_vector.x, vec.x)),
+      y: Math.max(State.min_vector.y, Math.min(State.max_vector.y, vec.y)),
+      z: Math.max(State.min_vector.z, Math.min(State.max_vector.z, vec.z))
+    };
   }
 }
