2298ca879d053a9cb5fde847db5b455a7f0c5f97
Daniel Taub <dmtaub@gmail.com>
2025-05-11 22:16:14 -0400

    remove fmn

--- src/keys.js
+++ src/keys.js
@@ -48,10 +48,6 @@ export function createKeys() {
       return changed;
     },
 
-    isPressed: function (key) {
-      return !!this.keys[key];
-    },
-
     clearChanges: function () {
       this.changedKeys = {};
     },
